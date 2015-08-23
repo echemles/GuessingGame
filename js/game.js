8082d3145dd35e498d09463de78053ab9a9c139b
lesterhm <lesterechem@gmail.com>
2015-08-22 20:06:07 -0400

    added guessing game & bootstrap on my GitHub

---
+++ js/game.js
@@ -0,0 +1,38 @@
+var chances = 5, answer = Math.ceil(Math.random()*100), input;
+
+function newGame(){
+  answer = Math.ceil(Math.random()*100);
+  chances = 5;
+  document.getElementById("chances").innerHTML = chances + " Guesses Remaining";
+  document.getElementById("status").innerHTML = "A new game has started.";
+  document.getElementById("guess2").reset();
+}
+
+function hint(){
+  input = Number(document.getElementById("guess").value);
+  if (input===NaN || input===0) {
+    document.getElementById("hint").innerHTML = "You might want to guess first."
+    return;
+  }
+  document.getElementById("hint").innerHTML = "You might want to add or subtract " + Math.abs(answer-input) + " to your guess."
+}
+
+function submit(){
+  input = Number(document.getElementById("guess").value);
+  if (input === NaN || input > 100 || input < 1) {
+    document.getElementById("status").innerHTML = "That is not a valid guess.\nEnter a number from 1 to 100."
+    return;
+  }
+  chances--;
+  if(chances<1) { 
+    alert("You have reached the maximum tries.");
+    return newGame();
+  } else if (input===answer) {
+    alert("You've guessed the number!");
+    return newGame();
+  } else {
+    document.getElementById("status").innerHTML = "Your guess is incorrect.\nPlease try again.";
+    document.getElementById("chances").innerHTML = chances + " Guesses Remaining";
+  }
+}
+
---
+++ js/test.js
@@ -0,0 +1,5 @@
+answer = Math.round(Math.random()*100)
+function showNumber(){
+  var x = document.getElementById("guess").value
+  alert("The number is " + answer);
+}
